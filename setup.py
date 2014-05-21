90865755f34f1fa2340215b30436c6af6046cee0
Nick Bagley <nick@thrivehive.com>
2014-05-21 14:44:07 -0400

    initial commit after packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name = 'pysendgrid',
+    packages = ['pysendgrid'],
+    version = '0.1',
+    author = 'Bruno Rocha',
+    author_email = 'nick@thrivehive.com',
+    url = 'https://github.com/teasea33/pysendgrid',
+    license = 'GPLv3',
+    classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Development Status :: 2 - Pre-Alpha"
+    ]
+    )
